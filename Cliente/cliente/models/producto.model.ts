83b269f44fbe0ac97680014abb2fce1298f35943
David Mora <davidjonamora@hotmail.es>
2025-03-29 17:19:23 -0500

    definición de modelos para producto y transacción

--- Cliente/cliente/models/producto.model.ts
+++ Cliente/cliente/models/producto.model.ts
@@ -1,12 +1,49 @@
-export interface Producto {
-    id?: number;
-    nombre: string;
-    descripcion: string;
-    categoria: string;
-    precio: number;
-    stock: number;
-    imagen: string; // Base64
-  }
+export interface ObtenerProductosPorId{
+  Id: number;
+  Ascendente: boolean;
+  NumeroRegistros: number;
+}
+
+export interface ObtenerProductosPorNombre{
+  Nombre: string;
+  Id: number;
+  Ascendente: boolean;
+  NumeroRegistros: number;
+}
+
+export interface ObtenerProducto{
+  Id: number
+}
+
+export interface validarInventario{
+  IdProducto: number;
+  StockDisminuir: number;
+}
+
+export interface AgregarProducto{
+  Nombre: string;
+  Descripcion:string;
+  Categoria:string;
+  Precio:number;
+  Stock:number;
+  Imagen:string;// Base64
+  
+}
+
+export interface ActualizarProducto {
+    Id: number;
+    Nombre: string;
+    Descripcion: string;
+    Categoria: string;
+    Precio: number;
+    Stock: number;
+    Imagen: string; // Base64
+}
+
+export interface EliminarProducto{
+  IdProducto:number;
+}
+
 
 
   
--- Cliente/cliente/models/transaccion.model.ts
+++ Cliente/cliente/models/transaccion.model.ts
@@ -1,11 +1,41 @@
-export interface Transaccion {
-    idTransaccion?: number;
-    idProducto: number;
-    fecha: string;
-    tipoTransaccion: 'COMPRA' | 'VENTA';
-    cantidad: number;
-    precioUnitario: number;
-    precioTotal: number;
-    detalle: string;
+export interface ObtenerTransaccionesPorIdProducto{
+  IdProducto: number;
+  IdTransaccion: number;
+  Ascendente: boolean;
+  NumeroRegistros: number;
+}
+
+export interface ObtenerTransaccionesPorFiltros{
+  IdProducto: number;
+  Fecha: Date;
+  TipoTransaccion: string;
+  IdTransaccion: number;
+  Ascendente: boolean;
+  NumeroRegistros: number;
+}
+
+export interface ObtenerTransaccion{
+  IdTransaccion: number;
+}
+
+export interface AgregarTransaccion{
+  IdProducto: number;
+  Fecha: Date;
+  TipoTransaccion: string;
+  Cantidad: Number;
+  PrecioUnitario: number;
+  PrecioTotal: number;
+  Detalle: string;
+}
+
+export interface ActualizarTransaccion {
+    IdTransaccion: number;
+    IdProducto: number;
+    Fecha: Date;
+    TipoTransaccion: 'COMPRA' | 'VENTA';
+    Cantidad: number;
+    PrecioUnitario: number;
+    PrecioTotal: number;
+    Detalle: string;
   }
   
